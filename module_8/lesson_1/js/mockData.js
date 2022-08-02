ee057bb6b06170948720efacdb109e5ca32d7fe5
Ivan Grigorev <82472551+Ivan-Grigorev@users.noreply.github.com>
2022-08-02 20:48:08 +0200

    Create mockData.js

---
+++ module_8/lesson_1/js/mockData.js
@@ -0,0 +1,64 @@
+const items = [
+  {
+    id: '8b7e4c38-af4a-4466-88a4-5abf1c359281',
+    text: 'Update todos styles',
+    isDone: true,
+    date: 1658428215947,
+  },
+  {
+    id: '39949407-7365-4dc1-9aed-7b074fdb41a1',
+    text: 'Repeat HTML',
+    isDone: true,
+    date: 1658428115947,
+  },
+  {
+    id: '98f9cbd9-90c0-4e7f-9619-00a330cfe6a0',
+    text: 'Repeat CSS',
+    isDone: true,
+    date: 1658427215947,
+  },
+  {
+    id: '42d1820a-ad57-4b65-b3c4-2138a4515fb8',
+    text: 'Buy milk',
+    isDone: false,
+    date: 1658418215947,
+  },
+  {
+    id: 'fa16d126-7ed9-4f20-89aa-817461b121cb',
+    text: 'Buy bread',
+    isDone: true,
+    date: 1658408215947,
+  },
+  {
+    id: 'd9f499c6-492d-449b-b7b3-93f2ee7b3aa5',
+    text: 'Throw out the trash',
+    isDone: false,
+    date: 1658308215947,
+  },
+  {
+    id: '4947c5f7-196a-49cb-8ddb-c322f30ed7d2',
+    text: 'Learn JS',
+    isDone: false,
+    date: 1658208215947,
+  },
+  {
+    id: '8e1156a9-e61b-44f6-a3ea-79ca1543da1e',
+    text: 'Learn React',
+    isDone: false,
+    date: 1658218215947,
+  },
+  {
+    id: '757dd4a0-76d2-4e9b-8452-8631a3d26907',
+    text: 'Pay for Internet',
+    isDone: false,
+    date: 1658217115947,
+  },
+  {
+    id: '8751d9ac-4388-4949-a838-da3cba07dba5',
+    text: 'Pay for phone',
+    isDone: false,
+    date: 1658117115947,
+  },
+];
+
+export default items;
